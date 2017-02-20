ab358d2328cbfc75ce3ace4a1fe8ed4f834dd958
unknown <a.akshabaev@c441a-10.kbtu.kz>
2017-02-20 11:50:32 +0600

    Examples of week 6

---
+++ Example1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Example1
+{
+    [Serializable]
+    public class Subject
+    {
+        public string name;
+        public Subject() { }
+        public Subject(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    [Serializable]
+    public class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public List<Subject> subjects;
+
+        public Student() {}
+        public Student(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            subjects = new List<Subject>();
+            subjects.Add(new Subject("Programming languages"));
+            subjects.Add(new Subject("Programming technologies"));
+        }
+    }
+
+    class Program
+    {
+        static void F1()
+        {
+            Student student = new Student("Almas", "Abeuov", 23);
+            FileStream fs = new FileStream("example.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            xs.Serialize(fs, student);
+            fs.Close();
+        }
+        static void F2()
+        {
+            FileStream fs = new FileStream("example.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            Student student = (Student) xs.Deserialize(fs);
+            Console.WriteLine(student.name + " " + student.surname);
+            Console.ReadKey();
+            fs.Close();
+        }
+
+        static void F3()
+        {
+            FileStream fs = new FileStream("example.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            Student student = new Student("Almas", "Abeuov", 23);
+            bf.Serialize(fs, student);
+            fs.Close();
+        }
+        static void F4()
+        {
+            FileStream fs = new FileStream("example.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            Student student = bf.Deserialize(fs) as Student;
+            foreach(Subject sub in student.subjects) {
+                Console.WriteLine(sub.name);
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            F4();
+        }
+    }
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Example2
+{
+    class Worker
+    {
+        bool isActive = true;
+        public void work()
+        {
+            int i = 0;
+            while (isActive) {
+                Console.WriteLine(i++);
+            }
+        }
+        public void stop()
+        {
+            isActive = false;
+        }
+    }
+
+    class Program
+    {
+        public static void func()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("thread #2 " + i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void F1()
+        {
+            Thread thread = new Thread(func);
+            thread.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Main thread " + i);
+                Thread.Sleep(0);
+            }
+            Console.ReadKey();
+
+        }
+
+        public static void F2()
+        {
+            Worker w = new Worker();
+            Thread t = new Thread(w.work);
+            t.Start();
+            Thread.Sleep(1000);
+            w.stop();
+            Console.ReadKey();
+        }
+
+        public class MyThread
+        {
+            public void func()
+            {
+                for (int i = 0; i < 100; i++)
+                    Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+            }
+            public MyThread(string threadName)
+            {
+                Thread thread = new Thread(this.func);
+                thread.Name = threadName;
+                thread.Start();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //MyThread[] threads = new MyThread[5];
+            MyThread thread1 = new MyThread("Thread 1");
+            MyThread thread2 = new MyThread("Thread 2");
+            MyThread thread3 = new MyThread("Thread 3");
+            MyThread thread4 = new MyThread("Thread 4");
+            MyThread thread5 = new MyThread("Thread 5");
+            MyThread thread6 = new MyThread("Thread 6");
+            Console.ReadKey();
+
+
+        }
+
+
+    }
+}
